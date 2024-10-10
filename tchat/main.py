95338e59292709267ec5956b766a69ce47f5c10b
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-09 22:45:04 -0400

    Refactor main.py to accept user input via command line arguments

--- tchat/main.py
+++ tchat/main.py
@@ -1,12 +1,6 @@
-import argparse
-
 def main():
-    parser = argparse.ArgumentParser(description='Process some inputs.')
-    parser.add_argument('--input', type=str, help='Input provided by the user')
-    
-    args = parser.parse_args()
-    
-    print(f'User input: {args.input}')
+    user_input = input(">>> ")
+    print(f"You entered: {user_input}")
 
 if __name__ == "__main__":
     main()
