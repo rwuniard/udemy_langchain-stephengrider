078f1f372b7014bbf76ee48c7d6ec8de59406a9f
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-16 22:27:05 -0400

    Refactor code to use ChromaDB for storing vector values and load and split text document using TextLoader and CharacterTextSplitter

--- facts/main.py
+++ facts/main.py
@@ -9,19 +9,33 @@ load_dotenv()
 
 embeddings = OpenAIEmbeddings()
 
-emb = embeddings.embed_query("Hello, how are you?")
-print(emb)
+# emb = embeddings.embed_query("Hello, how are you?")
+# print(emb)
 
-# splitter = CharacterTextSplitter(
-#     separator="\n",
-#     chunk_size=200,
-#     chunk_overlap=0
-# )
+splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+)
 
-# loader = TextLoader("facts.txt")
-# document = loader.load_and_split(
-#     text_splitter=splitter
-# )
+loader = TextLoader("facts.txt")
+document = loader.load_and_split(
+    text_splitter=splitter
+)
+
+db = Chroma.from_documents(
+    document,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+# The results is a tuple containing the document and the score
+results = db.similarity_search_with_score("What is an interesting fact about English language?")
+
+for result in results:
+    print("\n")
+    print(result[1]) # print the score
+    print(result[0].page_content) # print the content
 
 # for item in document:
 #     print(item.page_content, "\n")
