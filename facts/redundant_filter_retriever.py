ae712cb8d1421229551910581470ef10b9985072
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-20 22:13:12 -0400

    Created RedundantFilterRetriever to remove redundant embeddings inside Chroma.

---
+++ facts/redundant_filter_retriever.py
@@ -0,0 +1,22 @@
+from langchain.embeddings.base import Embeddings
+from langchain.vectorstores import Chroma
+from langchain.schema import BaseRetriever, Document
+from typing import List
+
+
+class RedundantFilterRetriever(BaseRetriever):
+    embeddings : Embeddings
+    chroma : Chroma 
+
+    def get_relevant_documents(self, query: str) -> List[Document]:
+        
+        # Calculate the embedding of the query
+        query_embedding = self.embeddings.embed_query(query)
+        # Take the query_embedding and feed it to the max_marginal_search_by_vector
+        results = self.embeddings.max_marginal_search_by_vector(query_embedding, lambda_mult=0.8)
+
+        return results
+    
+    async def aget_relevant_documents(self) -> List[Document]:
+        return []   
+    
