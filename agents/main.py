2ebddbed8d8502c782a73c549d26c75d20984f55
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-23 22:02:35 -0400

    Adding the SystemMessage, so the ChatGPT understands the database tables in the SQLite.

--- agents/main.py
+++ agents/main.py
@@ -5,8 +5,9 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
 )
 
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables
 
 from dotenv import load_dotenv
 
@@ -14,21 +15,31 @@ load_dotenv()
 
 chat = ChatOpenAI()
 
+tables = list_tables()
+# print(tables)
+
+# Create a prompt template for the agent
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to SQLite database, \n{tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
+# Create a list of tools for the agent
 tools = [run_query_tool]
 
+# Create an agent with the prompt and tools
 agent = OpenAIFunctionsAgent(
     llm=chat, 
     prompt=prompt,
     tools = tools
 )
 
+# Create an agent executor with the agent and tools
 agent_executor = AgentExecutor(
     agent=agent,
     tools=tools,
--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -4,6 +4,14 @@ from langchain.tools import Tool
 
 conn = sqlite3.connect("db.sqlite")
 
+# List all tables in the database
+def list_tables():
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = cursor.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+# Execute a SQL query and return the results
 def execute_sql(query: str) -> str:
     cursor = conn.cursor()
     try:    
@@ -12,6 +20,7 @@ def execute_sql(query: str) -> str:
     except sqlite3.OperationalError as err:
         return f"An error occurred: {str(err)}"
 
+# Create a tool for executing SQL queries
 run_query_tool = Tool.from_function(
     name="execute_sql",
     description="Execute a SQL query and return the results",
