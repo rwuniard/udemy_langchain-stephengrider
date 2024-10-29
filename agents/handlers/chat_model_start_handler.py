211d6a3ef02cc1153b038bc4b08a487c6802917d
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-28 21:12:45 -0400

    adding pyboxen

--- agents/handlers/chat_model_start_handler.py
+++ agents/handlers/chat_model_start_handler.py
@@ -1,6 +1,9 @@
 from typing import Any, Dict, List
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.messages import BaseMessage
+from pyboxen import boxen
+
+boxen("TEXT here", color="red", title= "Human Message")
 
 class ChatModelStartHandler(BaseCallbackHandler):
     def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[BaseMessage], **kwargs: Any) -> None:
