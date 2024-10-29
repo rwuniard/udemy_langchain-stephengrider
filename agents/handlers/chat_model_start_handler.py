b3f5ec3123a793238ed27a57ce7bcc6dedb82d4b
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-28 21:27:22 -0400

    print various messages from handlers

--- agents/handlers/chat_model_start_handler.py
+++ agents/handlers/chat_model_start_handler.py
@@ -3,9 +3,24 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.messages import BaseMessage
 from pyboxen import boxen
 
-boxen("TEXT here", color="red", title= "Human Message")
+
+def boxen_print(*args, **kwargs):
+    print(boxen(*args, **kwargs))
+
+boxen_print("TEXT here", color="red", title= "Human Message")
 
 class ChatModelStartHandler(BaseCallbackHandler):
     def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[BaseMessage], **kwargs: Any) -> None:
-        # print(f"Chat model started: {serialized}")
-        print(f"Messages: {messages}")
+        print("\n\n\n\n===== Sending messages to the model =====")
+        for message in messages[0]:
+            print(message.type)
+            if message.type == "system":
+                boxen_print(message.content, color="blue", title="System Message")
+            elif message.type == "human":
+                boxen_print(message.content, color="green", title="Human Message")
+            elif message.type == "ai":
+                boxen_print(message.content, color="yellow", title="AI Message")
+            elif message.type == "function":
+                boxen_print(message.content, color="magenta", title="Function Message")
+            else:
+                boxen_print(message.content, color="white", title="Unknown Message")
