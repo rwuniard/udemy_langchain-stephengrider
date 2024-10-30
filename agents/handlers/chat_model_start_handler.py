1aedd6f1afadf486d14521f2957037415c0beb09
Ronson Wuniardy <rwuniard@hotmail.com>
2024-10-29 21:39:15 -0400

    Changing the title to use message.type

--- agents/handlers/chat_model_start_handler.py
+++ agents/handlers/chat_model_start_handler.py
@@ -15,16 +15,16 @@ class ChatModelStartHandler(BaseCallbackHandler):
         for message in messages[0]:
             print(message.type)
             if message.type == "system":
-                boxen_print(message.content, color="blue", title="System Message")
+                boxen_print(message.content, color="blue", title=message.type)
             elif message.type == "human":
-                boxen_print(message.content, color="green", title="Human Message")
+                boxen_print(message.content, color="green", title=message.type)
             elif message.type == "ai" and "function_call" in message.additional_kwargs:
                 call = message.additional_kwargs["function_call"]
                 boxen_print(f"Function Call: {call}", color="yellow", title="AI Message with Function Call")
                 # boxen_print(message.content, color="yellow", title="AI Message with Function Call")
             elif message.type == "ai":
-                boxen_print(message.content, color="yellow", title="AI Message")
+                boxen_print(message.content, color="yellow", title=message.type)
             elif message.type == "function":
-                boxen_print(message.content, color="magenta", title="Function Message")
+                boxen_print(message.content, color="magenta", title=message.type)
             else:
                 boxen_print(message.content, color="white", title=message.type)
