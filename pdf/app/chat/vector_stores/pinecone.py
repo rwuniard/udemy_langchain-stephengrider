ca0308a03d8e043ef8693419eae7edc944c46ab1
Ronson Wuniardy <rwuniard@hotmail.com>
2024-11-11 17:23:09 -0500

    Build retriever

--- pdf/app/chat/vector_stores/pinecone.py
+++ pdf/app/chat/vector_stores/pinecone.py
@@ -1,9 +1,11 @@
 import os
+
 import pinecone
 # from langchain.vectorstores import Pinecone
 from langchain.vectorstores.pinecone import Pinecone
 
 from app.chat.embeddings.openai import embeddings
+from app.chat.models import ChatArgs
 
 pinecone.Pinecone(
     api_key=os.getenv("PINECONE_API_KEY"),
@@ -14,3 +16,10 @@ vector_store = Pinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), 
     embeddings
 )
+
+def build_retriever(chat_args: ChatArgs):
+    return vector_store.as_retriever(
+        search_kwargs={
+            "filter": {"pdf_id": chat_args.pdf_id}
+        }
+    )
