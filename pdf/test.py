59d704a9fe65c6629df379e8c7854fe675308b0b
Ronson Wuniardy <rwuniard@hotmail.com>
2024-11-14 22:14:55 -0500

    refactoring our chain and overriding the on_llm_new_token.

--- pdf/test.py
+++ pdf/test.py
@@ -1,23 +1,37 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+class StreamHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        print(f"Token: {token}")
+
 print ("Hello test")
 
-chat = ChatOpenAI(streaming=True)
+chat = ChatOpenAI(
+    streaming=True,
+    callbacks=[StreamHandler()]
+)
 
 prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
 ])
 
-messages = prompt.format_messages(content = "Tell me a joke")
+chain = LLMChain(llm=chat, prompt=prompt)
+
+for output in chain.stream({"content": "Tell me a joke"}):
+    print(output)
+
+# messages = prompt.format_messages(content = "Tell me a joke")
 # print(messages)
 
-for message in chat.stream(messages):
-    print(message)
+# for message in chat.stream(messages):
+#     print(message)
 # print(response)
 
 
